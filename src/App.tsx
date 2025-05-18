f4e14b313ee9c623128f6b6a1642721eb44f7088
nateazevedo <nathanielazevedo@hotmail.com>
2025-05-18 15:39:34 -0400

    remove router basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
-import './App.css';
-import { useMemo } from 'react';
-import Router from './features/Router';
-import { themeSettings } from './theme';
-import { useDispatch, useSelector } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { Box, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
-import { setModeInitial } from './features/labs/labsSlice';
+import "./App.css";
+import { useMemo } from "react";
+import Router from "./features/Router";
+import { themeSettings } from "./theme";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { Box, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { setModeInitial } from "./features/labs/labsSlice";
 
 const App = () => {
   const dispatch = useDispatch();
   const mode = useSelector((state: any) => state.lab.mode);
   if (!mode) {
-    const mode = localStorage.getItem('mode');
+    const mode = localStorage.getItem("mode");
     if (mode) {
       dispatch(setModeInitial(mode));
     } else {
-      dispatch(setModeInitial('dark'));
+      dispatch(setModeInitial("dark"));
     }
   }
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
-    <BrowserRouter basename='/labs'>
+    <BrowserRouter>
       <ThemeProvider theme={theme}>
         <Box
-          display='flex'
-          flexDirection='column'
-          sx={{ height: '100vh', width: '100vw' }}
+          display="flex"
+          flexDirection="column"
+          sx={{ height: "100vh", width: "100vw" }}
         >
           <CssBaseline />
           <Router />
